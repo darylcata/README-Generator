78582ec18e20a0d4c98b0cd821c478769282c88f
Darsing <catalunasiegfried@gmail.com>
2023-02-17 20:08:33 +0000

    added readme file structure

--- starter/index.js
+++ starter/index.js
@@ -5,17 +5,91 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your Github username?',
+        validate: function (userInput){
+            if(userInput === null)
+        return console.log("Please enter a username")}
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+        validate: function (userInput){
+            if(userInput === null)
+        return console.log("Please enter an email address")}
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is the name of your Github repo?',
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your Project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a brief description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What were the steps required for the Installation of your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How will this project be used?',
+    },
+    {
+        type: 'list',
+        message: 'Choose license used for your project',
+        name: 'license',
+        choices: ['MIT', 'Apache License 2.0'],
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can other developers contribute to your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How will this project be used?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What will be the command used to test the project?',
+        default: 'npm test'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What will be the command used to install the project?',
+        default: 'npm install'
+    },
 ];
+
 
 // function to write README file
 function writeToFile(fileName, data) {
+    const filename = `${data.name.toLowerCase().split(' ').join('')}.md`;
+
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+        err ? console.log(err) : console.log('Success!')
+    );
 }
 
 // function to initialize program
 function init() {
-
-}
+    inquirer.prompt(questions).then(writeToFile(data));
+};
 
 // function call to initialize program
 init();
--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,8 +1,115 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  // Generate Table of Contents conditionally based on userResponses
+  let TableOfContents = `## Table of Contents`;
 
-`;
+  if (data.installation !== '') {
+    TableOfContents += `
+ * [Installation](#installation)` };
+
+  if (data.usage !== '') {
+    TableOfContents += `
+ * [Usage](#usage)` };
+
+  if (data.contributing !== '') {
+    TableOfContents += `
+ * [Contributing](#contributing)` };
+
+  if (data.tests !== '') {
+    TableOfContents += `
+ * [Tests](#tests)` };
+
+
+  // This is how readme will look like
+  let markDown =
+
+    //license badge
+    `[![License](https://img.shields.io/badge/${data.license}-blue.svg)](https://opensource.org/licenses/${data.license})
+
+    # ${data.title}
+
+ ## Description 
+ 
+ Here is a brief description of my project:
+ 
+ ${data.description}`
+
+  // This will add table of contents to the readme file
+  markDown += TableOfContents;
+
+  // Optional Installation section
+  if (data.installation !== '') {
+
+    markDown +=
+
+      `## Installation
+ 
+These are the steps needed to install the project:
+ 
+ ${data.installation}`
+
+  };
+  // sage section
+  if (data.usage !== '') {
+
+    markDown +=
+
+      `## Usage 
+ ${data.usage}`
+  };
+ ` ## License
+ 
+  This project's license is ${data.license}
+  `;
+
+  // Optional Contributing section
+  if (data.contributing !== '') {
+
+    markDown +=
+
+`## Contributing
+ 
+For any contributions, please follow these guidelines:
+
+ ${data.contributing}`
+  };
+
+  // Optional Tests section
+  if (data.tests !== '') {
+
+    markDown +=
+      `
+ 
+ ## Tests
+ 
+ *Tests for application and how to run them:*
+ 
+ ${userResponses.tests}`
+  };
+
+  // Questions / About Developer section
+  let devInfo =
+    `
+ ---
+ 
+ ## Questions?
+
+ For any questions, please contact me with the information below:
+
+ GitHub: [github.com/${data.username}](${data.url})`;
+
+  // If GitHub email is not null, add to Developer section
+  if (data.email !== null) {
+
+    devInfo +=
+      `
+ Email: ${data.email}`};
+
+  // Add developer section to markdown
+  markDown += devInfo;
+
+  // Return markdown
+  return markDown;
 }
 
 module.exports = generateMarkdown;
